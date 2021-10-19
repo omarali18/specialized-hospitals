5891cd5674d0e4cd5a221c73ff23331f94547243
omarali18 <omarali1814000@gmail.com>
2021-10-19 08:21:20 +0600

    service details added

--- src/components/ServiceDetails/ServiceDetails.js
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -1,23 +1,81 @@
 import React, { useEffect, useState } from 'react';
-// import { useParams } from 'react-router';
-
+import "./ServiceDetails.css"
 
 const ServiceDetails = () => {
-    const [services, setServices] = useState([]);
-    // const id = useParams()
 
-    useEffect(() => {
-        // fetch("fakeServices.json")
-        fetch("fakeServices.json")
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                setServices(data)
-            })
-    }, [])
     return (
-        <div>
-            <h1>this is doctors</h1>
+        <div className=" w-75 mx-auto mb-5 mt-5">
+            <h1 className="text-center">Top Categories</h1>
+            <div className="row">
+                <div className="col-md-3 card-size g-5">
+                    <div className="card bg-dark text-white ">
+                        <img src="https://www.wanderglobe.org/wp-content/uploads/2020/02/ent-doctor.jpg" className="card-img" alt="..." />
+                        <div className="card-img-overlay img-text-contgainer">
+                            <h5 className="card-title img-text d-inline">ENT</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 card-size g-5">
+                    <div className="card bg-dark text-white col-md-3">
+                        <img src="http://med.stanford.edu/content/dam/sm-news/images/2014/06/kidney-stock.jpg" className="card-img" alt="..." />
+                        <div className="card-img-overlay img-text-contgainer">
+                            <h5 className="card-title img-text ">Kidney & Medicine</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 card-size g-5">
+                    <div className="card bg-dark text-white col-md-3">
+                        <img src="https://i2.wp.com/www.finddoctor24.com/wp-content/uploads/2017/09/Neuromedicine.png?fit=1364%2C766&ssl=1" className="card-img" alt="..." />
+                        <div className="card-img-overlay img-text-contgainer">
+                            <h5 className="card-title img-text">Neuromedicine</h5>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 card-size g-5">
+                    <div className="card bg-dark text-white col-md-3">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa4UWe3dN_kPrZiQTVhTxNx9aKJisB5cCRLA&usqp=CAU" className="card-img" alt="..." />
+                        <div className="card-img-overlay img-text-contgainer">
+                            <h5 className="card-title img-text ">Orthopaedic Surgeon</h5>
+                        </div>
+                    </div>
+                </div>
+                {/* ================== */}
+                <div className="col-md-3 card-size g-5">
+                    <div className="card bg-dark text-white col-md-3">
+                        <img src="https://ekbalpurnursinghome.org/wp-content/uploads/2020/10/gastroenterology.png" className="card-img" alt="..." />
+                        <div className="card-img-overlay img-text-contgainer">
+                            <h5 className="card-title img-text ">Gastroenterology</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 card-size g-5">
+                    <div className="card bg-dark text-white col-md-3">
+                        <img src="https://i1.wp.com/www.finddoctor24.com/wp-content/uploads/2017/11/find-doctor-24.jpg?fit=616%2C346&ssl=1" className="card-img" alt="..." />
+                        <div className="card-img-overlay img-text-contgainer">
+                            <h5 className="card-title img-text ">Medicine</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 card-size g-5">
+                    <div className="card bg-dark text-white col-md-3">
+                        <img src="http://www.northshore.org/globalassets/healthy-you/blog/2012-2014/pediatric-appointments.jpg" className="card-img" alt="..." />
+                        <div className="card-img-overlay img-text-contgainer">
+                            <h5 className="card-title img-text ">Pediatrics</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 card-size g-5">
+                    <div className="card bg-dark text-white col-md-3">
+                        <img src="http://aarogyadhamhospital.com/wp-content/uploads/2018/05/5a3a43452cc09e078496e8ca.jpg" className="card-img" alt="..." />
+                        <div className="card-img-overlay img-text-contgainer">
+                            <h5 className="card-title img-text ">Chest & Medicine</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
